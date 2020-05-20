f1ccae09a55f34e48c766276893488e9d8ff334a
Leonardo-Flores <57921586+Leonardo-Flores@users.noreply.github.com>
2020-05-20 03:17:06 -0300

    Resolução exercicios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [projects, SetProjects] = useState([])
+
+  useEffect(() => {
+    api.get('projects')
+    .then(response => {
+      SetProjects(response.data)
+    });   
+  }, [])
+
+  
   async function handleAddRepository() {
     // TODO
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now}`,
+      owner: 'Leonardo Flores'
+    })
+
+    const project = response.data
+
+    SetProjects([...projects, project])
+  
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    return await api.delete(`projects/${id}`);
+
+    
   }
 
   return (
@@ -17,7 +41,7 @@ function App() {
         <li>
           Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository('1')}>
             Remover
           </button>
         </li>
